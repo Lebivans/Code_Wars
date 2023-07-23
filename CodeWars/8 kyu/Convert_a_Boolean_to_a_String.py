133de39f51e70ff4d9e93a179aee80ec65a94004
Ivan <Lebivans@mail.ru>
2023-07-23 17:13:47 +0300

    23.07.2023_2

---
+++ CodeWars/8 kyu/Convert_a_Boolean_to_a_String.py
@@ -0,0 +1,23 @@
+'''
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+'''
+
+def boolean_to_string(b):
+    if b == True:
+        return 'True'  
+    else:
+        return 'False'
+
+
+def boolean_to_string(b):
+    return str(b)
+
+
+def boolean_to_string(b):
+    return 'True' if b else 'False'
+
+
+def boolean_to_string(b):
+    if b:
+        return "True"
+    return "False"
