28c0ed4b5bac98df862170e8eafd6a1dc1029aab
Ivan <Lebivans@mail.ru>
2023-07-25 10:39:53 +0300

    25.07.2023

---
+++ CodeWars/8 kyu/Invert_values.py
@@ -0,0 +1,10 @@
+'''
+https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/python
+'''
+
+def invert(array):
+    return [i * (-1) for i in array]
+
+
+def invert(lst):
+    return [-x for x in lst]
